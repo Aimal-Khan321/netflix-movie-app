cde2929f33991de47fc360a1a0b62f521df06545
Aimal-khan321 <aimal.khan02676@gmail.com>
2023-04-21 19:12:38 +0400

    commit message

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.scss';
+import Home from './pages/Home/Home'; // eslint-disable-next-line
+import Watch from './pages/watch/Watch'; // eslint-disable-next-line
+import Register from './pages/register/Register'; 
+import Login from './pages/login/Login'; // eslint-disable-next-line
+function App() {
+  return <Register/>
+}
+
+export default App;
